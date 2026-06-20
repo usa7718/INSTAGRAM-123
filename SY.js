6b8c5ab86232614e46d75d006a86daf9df5eb668
usa7718 <usa.sabir.7718@gmail.com>
2026-06-20 16:14:16 +0530

    Update URL in response to new endpoint

--- SY.js
+++ SY.js
@@ -113,7 +113,7 @@ S7LOVESY.get('/create-link', (req, res) => {
 
             res.json({
                 success: true,
-                url: `https://instagram-123.onrender.com/${LovesNameSY}`
+                url: `https://instagram-321.onrender.com/${LovesNameSY}`
             });
 
         } catch (writeErr) {
